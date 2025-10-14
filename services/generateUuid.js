a42333a01059e6cf08f0ee5d844b0fde51c9577f
JimmyCodeMunity <jameswafula2002@gmail.com>
2025-10-14 15:05:16 +0300

    random id from crypto

--- services/generateUuid.js
+++ services/generateUuid.js
@@ -1,5 +1,7 @@
 // import { v4 as uuidv4 } from "uuid";
 
+import { randomUUID } from "crypto";
+
 // export function generateUUID() {
 //   const newUUID = uuidv4();
 //   console.log("Generated UUID:", newUUID);
@@ -8,3 +10,11 @@
 
 // // Call the function to generate a UUID
 // // generateUUID();
+
+export function generateUUID() {
+  const newUUID = randomUUID();
+  console.log("Generated UUID:", newUUID);
+  return newUUID;
+}
+
+// generateUUID();
